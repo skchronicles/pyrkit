5db764967003566172795c057096d0b9214f4b39
skchronicles <kuhnsa2@vcu.edu>
2020-08-20 15:55:36 -0400

    Adding data catalogue parser and merging PI cells in template spreadsheet

--- src/uploader.py
+++ src/uploader.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import print_function, division
 import sys, os, re
 import pandas as pd
@@ -6,7 +7,21 @@ import pandas as pd
 # Configuration for defining valid sheets and other default values
 config = {
     ".warning": ["\033[93m", "\033[00m"], ".error": ["\033[91m", "\033[00m"],
-    ".sheets": ["Data Dictionary", "Project Template", "Sample Template"]
+    ".sheets": ["Data Dictionary", "Project Template", "Sample Template"],
+    "data_dictionary": {
+        "sheet_name": "Data Dictionary",
+        "skip_lines": [0],
+        "order": ["collection_type", "is_required", "field_name", "dme_name"],
+        "index": {
+            "collection_type": 0,
+            "is_required": 0,
+            "field_name": 1,
+            "dme_name": 2,
+            "description": 3,
+            "example": 4
+        }
+    }
+
 }
 
 
@@ -61,9 +76,10 @@ def path_exists(path):
     If the PATH does not exist, it will attempt to create the directory and it's parent
     directories.
     """
+    cstart, cend = config['.warning']
+
     if not os.path.isdir(path):
-        print("\n{}WARNING: Provided output directory does not exist!{}".format(*config['.warning']), file=sys.stderr)
-        print("Creating output directory {}".format(path), file=sys.stderr)
+        print("{}WARNING:{} Output directory '{}' does not exist... creating it now!".format(cstart, cend, path), file=sys.stderr)
         try:
             os.makedirs(path)
         except OSError as e:
@@ -118,22 +134,80 @@ def validate(user_inputs):
 
     return meta_sheet, output_path, sheets
 
-def data_dictionary():
+
+def _parsed_meta(excel_df, indexes):
+    """Private function for 'meta_dictionary' to parse the Data Dictionary sheet.
+    This function generates the following parsed values: collection_type, is_required,
+    field_name, dme_name.
     """
+    metadata = {}
+    for i, row in excel_df.iterrows():
+        # Remove any leading or trailing whitespace and parse the columns of interest
+        collection_name, is_required, field_name, dme_name = [str(row[index]).lstrip().rstrip() for index in indexes]
+        # skip over over empty lines or nan values
+        if not is_required or is_required == 'nan':
+            continue
+        # Get collection type: PI, Project, Sample
+        elif "collection" in collection_name.lower():
+            collection_type = collection_name.split()[0]
+            continue
+
+        yield collection_type, is_required, field_name, dme_name
+
+
+def meta(sheet, spreadsheet, order, index, log_route):
+    """Parses the 'Data Dictionary' sheet located in the project_request_spreadsheet.
+    Returns a nested dictionary where [key1] = collection_type (PI, Project, Sample),
+    [key2] = field and the value is a list [dme_name, is_requred]. A log file get
+    created in '{user-defined-outpath}/logs/data_dictionary.txt'.
+    """
+    skipover = config["data_dictionary"]["skip_lines"]
+    metadata = {}
+
+    # Skip over reading the first line or header
+    df = pd.read_excel(spreadsheet, sheet_name=sheet, header=None, skiprows=skipover)
+    # Creating logging output file
+    outfh = open(os.path.join(log_route, "data_dictionary.txt"), "w")
+
+    # Get sorted indices of important fields to parse
+    indices = [index[f] for f in order]
+    for col, req, field, dme in _parsed_meta(df, indices):
+        outfh.write("{}\t{}\t{}\t{}\n".format(col, req, field, dme))
+        if col not in metadata:
+            metadata[col] = {}
+
+        metadata[col][field] = [dme, req]
+
+    return metadata
+
+def project():
+    """Parses the 'Project Template' sheet in the project_request_spreadsheet
+    to extract PI-level and Project-level metadata. Returns a nested dictionary where
+    [key1] = collection_type (PI, Project), [key2] = field, and the value is a list
+    of values where each value is metadata for a sub-project [Proj-1_attr, Proj-2_attr].
+    A log file gets created in '{user-defined-outpath}/logs/project_template.txt'.
     """
     pass
-    
+
 def main():
 
     # @args(): Parses positional command-line args
     # @validate(): Checks if user inputs are vaild
     metadata, opath, sheets = validate(args(sys.argv))
 
-    # Generate Data Dictionary
+    # Log file directory
+    logs = os.path.join(opath, "logs")
+    path_exists(logs)
 
+    # Get specification for parsing 'Data Dictionary'
+    data_catelog = config["data_dictionary"]["sheet_name"]
+    sort = config["data_dictionary"]["order"]
+    indices = config["data_dictionary"]["index"]
 
+    # Generate Data Dictionary: dict[collection_type][field_name] = list(dme_name, is_required)
+    meta_dictionary = meta(sheet = data_catelog, spreadsheet = metadata, order=sort, index=indices, log_route=logs)
 
-
+    # Get all project metadata from Project Template
 
 
 if __name__ == '__main__':
